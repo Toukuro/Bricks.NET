1af84f8472a494005fddbc02030eec8dd2e47933
Toukuro <kj_adachi@nifty.com>
2019-05-02 08:22:24 +0900

    実装中

--- src/Bricks.NET/BricksCore/Logger/LogAccessor/LogAccessor.vb
+++ src/Bricks.NET/BricksCore/Logger/LogAccessor/LogAccessor.vb
@@ -1,3 +1,27 @@
-﻿Public Class LogAccessor
+﻿Namespace Logging
 
-End Class
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    Public MustInherit Class LogAccessor
+
+        Public Overridable Sub Open()
+
+        End Sub
+
+        Public Overridable Sub Close()
+
+        End Sub
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="iMsg"></param>
+        ''' <param name="iLevel"></param>
+        Public Overridable Sub Write(iMsg As String, Optional iLevel As OutputLevelEnum = OutputLevelEnum.All)
+
+        End Sub
+
+    End Class
+
+End Namespace
---
+++ src/Bricks.NET/BricksCore/Logger/LogAccessor/NullAccessor.vb
@@ -0,0 +1,13 @@
+﻿Namespace Logging
+
+    ''' <summary>
+    ''' NULLデバイスアクセサ
+    ''' 
+    ''' 何も出力しないアクセサ
+    ''' </summary>
+    Public Class NullAccessor
+        Inherits LogAccessor
+
+    End Class
+
+End Namespace
